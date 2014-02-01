d7d4ff744dd0cab7231154ecbfd8cbaa51a90530
Nicholas Furness <nfurness@nixta.com>
2014-02-01 11:32:24 -0500

    Some refactoring and hooking into the proper Map onLoad event (rather than document ready).

--- js/arcgis.js
+++ js/arcgis.js
@@ -1,8 +1,15 @@
 /* Uses pre-existing library - http://github.com/esri/bootstrap-map-js */
 require(["esri/map", "esri/dijit/Scalebar", "http://esri.github.io/bootstrap-map-js/src/js/bootstrapmap.js", "dojo/domReady!"], 
   function(Map, Scalebar, BootstrapMap) {
-    var mapLocations, timerHandle, secTimer;
-    var index = 0, countDown = 5, sec = countDown, t = countDown*1000, playing = false;
+    var mapLocations = [
+        ["gray",[-100,45],3], /* World*/ ["streets",[-0.13,51.50],11], // London
+        ["hybrid",[151.21,-33.87],14], /* Sydney */ ["topo",[-77.017,38.943],17], // D.C.
+        ["national-geographic",[-84.0,10],9], /* Costa Rica */["oceans",[-40,30],4], // Atlantic   
+        ["gray",[135,-25],4], /* Australia */ ["streets",[-117.20,32.73],13], // San Diego
+        ["hybrid",[-77.65,24.20],9], /* Bahamas */ ["topo",[139.75,35.69],17], // Tokyo
+        ["national-geographic",[-74,40.74],12], /* New York */ ["oceans",[-160,30],3] // Pacific
+      ];
+    var index = 0, countDown = 5, sec = countDown, playing = false, secTimer;
     var map = new Map("mapDiv",{
       basemap:"national-geographic",
       center:[-122.45,37.77],
@@ -18,13 +25,16 @@ require(["esri/map", "esri/dijit/Scalebar", "http://esri.github.io/bootstrap-map
         nextMap(false);
       }
     });
+    map.on("load", function(e) {
+      updateBasemapUI($("#mapDiv").data("basemap"));
+    });
     map.on("basemap-change", function(e) {
       updateBasemapUI(e.current.basemapName);
     });
     // Functions
     function updateBasemapUI(basemapType) {
-      $("#navbar > li > a").removeClass("selected-basemap");
-      $("#navbar > li > a[data-basemap='" + basemapType + "']").addClass("selected-basemap");
+      $("#navbar a").removeClass("selected-basemap");
+      $("#navbar a[data-basemap='" + basemapType + "']").addClass("selected-basemap");
     }
     function showCountdown(secondsLeft) {
       secondsLeft===""?$("#start-countdown").hide():$("#start-countdown").show();
@@ -32,14 +42,15 @@ require(["esri/map", "esri/dijit/Scalebar", "http://esri.github.io/bootstrap-map
     }
     function nextMap(startUp) {
       clearInterval(secTimer);
-      if (startUp) {
-        showCountdown(sec); // updated immediately
+      if (startUp) { // updated immediately
+        showBasemap(index);
+        showCountdown(sec); 
       }
       secTimer = setInterval (function() {
         sec--;
         if (sec == 0) {
           index = (index < (mapLocations.length - 1) ? (index + 1) : 0);
-          showBasemap(index, true);
+          showBasemap(index);
           sec = countDown;
         }
         showCountdown(sec);
@@ -53,34 +64,20 @@ require(["esri/map", "esri/dijit/Scalebar", "http://esri.github.io/bootstrap-map
       }
       playing = !playing;
     }
-    function initBasemaps() {
-      mapLocations = [
-        ["gray",[-100,45],3], /* World*/ ["streets",[-0.13,51.50],11], // London
-        ["hybrid",[151.21,-33.87],14], /* Sydney */ ["topo",[-77.017,38.943],17], // D.C.
-        ["national-geographic",[-84.0,10],9], /* Costa Rica */["oceans",[-40,30],4], // Atlantic   
-        ["gray",[135,-25],4], /* Australia */ ["streets",[-117.20,32.73],13], // San Diego
-        ["hybrid",[-77.65,24.20],9], /* Bahamas */ ["topo",[139.75,35.69],17], // Tokyo
-        ["national-geographic",[-74,40.74],12], /* New York */ ["oceans",[-160,30],3] // Pacific
-      ];        
-    }
-    function setBasemap(basemapType) { // Disable playmode
+    function setBasemap(basemapType) {
       clearInterval(secTimer);
       map.setBasemap(basemapType);
     }
-    function showBasemap(index,moveLocation) { // set map and location
+    function showBasemap(index) { // set map and location
       setBasemap(mapLocations[index][0]);
-      if (moveLocation) {
-        map.centerAndZoom(mapLocations[index][1],mapLocations[index][2]);
-      }
+      map.centerAndZoom(mapLocations[index][1],mapLocations[index][2]);
     }
     // Bootstrap stuff
     $(document).ready(function() {
-      initBasemaps();
-      updateBasemapUI($("#mapDiv").data("basemap"));
       $("#navbar li").click(function(e) {
-        if (playing) toggleTour();
-        setBasemap(e.target.dataset.basemap);
-        if ($(".navbar-collapse.in").length > 0) {
+        if (playing) toggleTour(); // Stop playing
+        setBasemap(e.target.dataset.basemap); // Set the basemap
+        if ($(".navbar-collapse.in").length > 0) { // Hide if showing responsive menu
           $(".navbar-toggle").click();
         }
       });
